cac8cf43e7541ad838656f41fc9822e07c2ef36c
nemnem202 <naimelhabbas@gmail.com>
2025-09-12 12:02:33 +0200

    added dotenv

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,6 +1,9 @@
 import { Express } from "express";
 import express from "express";
 import router from "./router";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app: Express = express();
 const port = 3000;
@@ -8,5 +11,5 @@ const port = 3000;
 app.use(router);
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${process.env.PORT}`);
 });
