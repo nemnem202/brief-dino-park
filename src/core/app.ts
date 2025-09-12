0b12861df420d8dddd1f35cadb97de5e89a65b13
nemnem202 <naimelhabbas@gmail.com>
2025-09-12 11:59:15 +0200

    initialiser router

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,27 +1,12 @@
 import { Express } from "express";
 import express from "express";
 import { Database } from "./db";
+import router from "./router";
 
 const app: Express = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, Dino Park!");
-});
-
-const init_database = async () => {
-  const db = Database.getInstance();
-
-  await db.user.createMany({
-    data: [{ email: "oseose", name: "john" }],
-  });
-
-  const users = await db.user.findMany();
-
-  console.log(users);
-};
-
-init_database();
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
