12ba8082440353be21265d04db72094e9babb9c0
nemnem202 <naimelhabbas@gmail.com>
2025-09-12 12:32:01 +0200

    added ejs pages and related routes

--- src/core/router.ts
+++ src/core/router.ts
@@ -2,8 +2,36 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => {
+router.get("/", (_, res) => {
   res.render("home.ejs");
+});
+
+router.get("/board", (req, res) => {
+  res.render("board.ejs");
+});
+
+router.get("/reservations", (_, res) => {
+  res.render("reservation_page.ejs");
+});
+
+router.get("/reservations/:reservation_id", (req, res) => {
+  res.redirect("/payment/" + req.params.reservation_id);
+});
+
+router.get("/payment/:reservation_id", (req, res) => {
+  res.render("payment.ejs", { reservation_id: req.params.reservation_id });
+});
+
+router.get("/sign-up", (_, res) => {
+  res.render("sign_up.ejs");
+});
+
+router.get("/sign-in", (_, res) => {
+  res.render("sign_in.ejs");
+});
+
+router.use((_, res) => {
+  res.render("not_found.ejs");
 });
 
 export default router;
