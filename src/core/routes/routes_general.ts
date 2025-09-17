233153be53e9563bc75aa19173a951b2b8295b39
nemnem202 <naimelhabbas@gmail.com>
2025-09-17 15:06:44 +0200

    added auth user middelware

--- src/core/routes/routes_general.ts
+++ src/core/routes/routes_general.ts
@@ -1,11 +1,19 @@
 import { Router } from "express";
+import { MiddleWare } from "../../libs/middleware";
 
 const general_routes = Router();
 
-general_routes.get("/", (_, res) => {
-  console.log("[REQUEST] : home");
-  res.render("home.ejs");
-});
+general_routes.get(
+  "/",
+  (req, res, next) => MiddleWare.check_user_auth(req, res, next),
+  (req, res) => {
+    if (req.user_id) {
+      console.log("[USER] is connected", req.user_id);
+    }
+    console.log("[REQUEST] : home");
+    res.render("home.ejs");
+  }
+);
 
 general_routes.get("/reservations", (_, res) => {
   res.render("reservation_page.ejs");
--- src/libs/middleware.ts
+++ src/libs/middleware.ts
@@ -1,6 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import CookieGen from "./cookie_gen";
 
+declare module "express-serve-static-core" {
+  interface Request {
+    user_id?: string;
+  }
+}
+
 export class MiddleWare {
   static async check_admin_auth(req: Request, res: Response, next: NextFunction) {
     console.log("[ADMIN AUTH ATTEMPT]");
@@ -18,6 +24,23 @@ export class MiddleWare {
     } catch (err) {
       res.status(400);
       res.send({ message: "auth_token missing !" });
+    }
+  }
+
+  static async check_user_auth(req: Request, res: Response, next: NextFunction) {
+    console.log("[USER AUTH ATTEMPT]");
+
+    try {
+      const token = req.cookies.user_token;
+      const id = CookieGen.check_user_id_from_token(token);
+      if (id) {
+        req.user_id = id;
+        next();
+      } else {
+        next();
+      }
+    } catch (err) {
+      next();
     }
   }
 
